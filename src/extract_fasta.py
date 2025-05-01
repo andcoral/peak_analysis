81d8ff63da3cfa0fe4f515580f38a46ef2e43f44
Andromeda Coral G <andcoral@lcg.unam.mx>
2025-05-01 14:21:54 -0600

    Creacion de script del modulo A del proyecto con la estructura dada.

---
+++ src/extract_fasta.py
@@ -0,0 +1,13 @@
+def cargar_genoma(fasta_path):
+    """Carga el genoma desde un archivo FASTA y devuelve una única cadena de texto."""
+
+def leer_archivo_picos(peaks_path):
+    """Lee el archivo de picos y devuelve una lista de diccionarios con TF_name, start y end."""
+
+def extraer_secuencias(peaks_data, genoma):
+    """Agrupa las secuencias extraídas por TF_name en un diccionario."""
+
+def guardar_fasta_por_tf(secuencias_por_tf, output_dir):
+    """Guarda archivos FASTA separados por cada TF_name."""
+
+"""Principal que orquesta la ejecución del script."""
