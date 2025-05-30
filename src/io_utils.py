91512e7f224bc22db1ce6d317b3e629141f7adab
Andromeda Coral G <andcoral@lcg.unam.mx>
2025-05-29 23:51:23 -0600

    Modificacion de docstring y variables en io_utils.py para mejor entendimiento.

--- src/io_utils.py
+++ src/io_utils.py
@@ -1,21 +1,22 @@
 """
- Funciones auxiliares de escritura de archivos FASTA
+ Funciones auxiliares de escritura de archivos FASTA. A partir de diccionarios previamente creados, 
+ genera un archivo por cada par de clave y valor.
  """
 
 import os
 
-def guardar_fasta_por_tf(secuencias_por_tf, output_dir):
+def guardar_fasta_por_tf(secuencias_por_tf, output_dir): # Parametos: diccionario y ruta de salida
     """Guarda archivos FASTA separados por cada TF_name."""
 
     if not os.path.exists(output_dir): # Precaucion
-        os.makedirs(output_dir)
+        os.makedirs(output_dir) # Crea la carpeta de salida en caso de que no exista
 
     for tf, secuencias in secuencias_por_tf.items(): # Recorre diccionarios
-        file_path = os.path.join(output_dir, f"{tf}.fa") # Ruta del archivo a crear
+        file_path = os.path.join(output_dir, f"{tf}.fa") # Ruta y nombre del archivo a crear
         try: # Precaucion permisos
-            with open(file_path, 'w') as f:
+            with open(file_path, 'w') as file:
                 for header, secuencia in secuencias:
-                    f.write(f">{header}\n")
-                    f.write(f"{secuencia}\n")
+                    file.write(f">{header}\n")
+                    file.write(f"{secuencia}\n")
         except IOError:
             print(f"Error al escribir el archivo: {file_path}")
